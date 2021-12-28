3c5f402ee97c73f22dd8a5b7fa96f27cf934aa8d
Javier Gatón Herguedas <javigaton@gmail.com>
2021-12-28 12:14:18 +0100

    Refactorized now modularized in different functions

--- Bot.py
+++ Bot.py
@@ -2,40 +2,61 @@ import tweepy
 import json
 import time
 
-# Read JSON
-f = open("auth_data.json", 'r')
-data = json.load(f)
-consumer_key = data['CONSUMER_KEY']
-consumer_secret = data['CONSUMER_SECRET']
-access_token = data['ACCESS_TOKEN']
-access_token_secret = data['ACCESS_TOKEN_SECRET']
+class AuthData:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-#api.update_status("Soy un bot")
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+    @staticmethod
+    def CreateFromJson(fileLocation: str) -> 'AuthData': 
+        f = open(fileLocation, 'r')
+        data = json.load(f)
+        consumer_key = data['CONSUMER_KEY']
+        consumer_secret = data['CONSUMER_SECRET']
+        access_token = data['ACCESS_TOKEN']
+        access_token_secret = data['ACCESS_TOKEN_SECRET']
+        return AuthData(consumer_key, consumer_secret, access_token, access_token_secret)
 
-tweets = list(tweepy.Cursor(api.search_tweets,"#domingosanitario").items(30))
-for tweet in tweets:
+def retweetKeyword(api: tweepy.API, keyword: str, items: int):
+    tweets = list(tweepy.Cursor(api.search_tweets, keyword).items(items))
+    for tweet in tweets:
+        try:
+            print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+
+            tweet.retweet()
+            print('Retweet published successfully.')
+
+            # Read Twitter's rules on automation. Don't spam!
+            time.sleep(2)
+
+        # Some basic error handling. Will print out why retweet failed, into your terminal.
+        except tweepy.TweepyException as error:
+            print('\nError TweepyException. Retweet not successful. Reason: ')
+            print(error)
+
+        except tweepy.HTTPException as error:
+            print('\nError HTTPException. Retweet not successful. Reason: ')
+            print(error)
+
+        except StopIteration:
+            break
+
+def main():
+    # Read auth data
+    ad = AuthData.CreateFromJson("auth_data.json")
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(ad.consumer_key, ad.consumer_secret)
+    auth.set_access_token(ad.access_token, ad.access_token_secret)
+    api = tweepy.API(auth)
     try:
-        print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Error during authentication")
+    # Retweet some tweets with the hashtag
+    retweetKeyword(api, "#domingosanitario", 30)
 
-        tweet.retweet()
-        print('Retweet published successfully.')
-
-        # Read Twitter's rules on automation. Don't spam!
-        time.sleep(2)
-
-    # Some basic error handling. Will print out why retweet failed, into your terminal.
-    except tweepy.TweepyException as error:
-        print('\nError. Retweet not successful. Reason: ')
-        print(error)
-
-    except StopIteration:
-        break
+if __name__=="__main__":
+    main()
