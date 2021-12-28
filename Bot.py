2036118b9b75f3c9b614649b05f4be47b5795345
Pablo Martínez <pablomartinez1017@gmail.com>
2021-12-28 11:45:47 +0100

    New approach

--- Bot.py
+++ Bot.py
@@ -8,47 +8,41 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
-class RetweetListener(tweepy.Stream):
-    def __init__(self, api):
-        self.api = api
-    def on_status(self, tweet):
-        logger.info(f"Processing tweet id {tweet.id}")
-        if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id:
-            # This tweet is a reply or I'm its author so, ignore it
-            return
-        if not tweet.retweeted:
-            # Retweet, since we have not retweeted it yet
-            try:
-                tweet.retweet()
-            except Exception as e:
-                logger.error("Error on fav and retweet", exc_info=True)
+# Read JSON
+f = open("auth_data.json", 'r')
+data = json.load(f)
+consumer_key = data['CONSUMER_KEY']
+consumer_secret = data['CONSUMER_SECRET']
+access_token = data['ACCESS_TOKEN']
+access_token_secret = data['ACCESS_TOKEN_SECRET']
 
-    def on_error(self, status):
-        logger.error(status)
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+#api.update_status("Soy un bot")
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
 
-def main(keywords):
-    # Read JSON
-    f = open("auth_data.json", 'r')
-    data = json.load(f)
-    consumer_key = data['CONSUMER_KEY']
-    consumer_secret = data['CONSUMER_SECRET']
-    access_token = data['ACCESS_TOKEN']
-    access_token_secret = data['ACCESS_TOKEN_SECRET']
-
-    # Authenticate to Twitter
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    #api.update_status("Soy un bot")
-    tweets_listener = RetweetListener(api)
-    stream = tweepy.Stream(consumer_key,consumer_secret,access_token,access_token_secret)
-    stream.filter(track=keywords, languages=["es"])
+for tweet in list(tweepy.Cursor(api.search_tweets("#domingosanitario")).items(10)):
     try:
-        api.verify_credentials()
-        print("Authentication OK")
-    except:
-        print("Error during authentication")
+        print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
 
-if __name__ == "__main__":
-    main(["#domingosanitario"])
+        tweet.retweet()
+        print('Retweet published successfully.')
+
+        # Where sleep(10), sleep is measured in seconds.
+        # Change 10 to amount of seconds you want to have in-between retweets.
+        # Read Twitter's rules on automation. Don't spam!
+        sleep(1)
+
+    # Some basic error handling. Will print out why retweet failed, into your terminal.
+    except tweepy.TweepError as error:
+        print('\nError. Retweet not successful. Reason: ')
+        print(error.reason)
+
+    except StopIteration:
+        break
